0c091456f88136ce4ebe39e991632778d69219d9
SadalKhalid <Sadal.14@hotmail.com>
2016-10-26 23:50:08 +0300

    add all my files

--- Wiki/admin.py
+++ Wiki/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Profile, Article,Activity
 
-# Register your models here.
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user','mobile_number']
+
+  class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status','addition_date','is_it_translated']
+    ordering = ['title']
+    actions = [make_published]
+
+  def make_published(self,request, queryset):
+        rows_updated = queryset.update(status='p')
+        if rows_updated == 1:
+            message_bit = "1 story was"
+        else:
+            message_bit = "%s articles were" % rows_updated
+        self.message_user(request, "%s successfully marked as published." % message_bit)
+
+class ActivityAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Article,ArticleAdmin)
+admin.site.register(Activity,ActivityAdmin)
--- Wiki/models.py
+++ Wiki/models.py
@@ -1,5 +1,47 @@
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    admin = models.ForeignKey(User, limit_choices_to={'is_staff': True})
+    user = models.OneToOneField(User, related_name='profile')
+    ar_first_name = models.CharField(max_length=30,
+                                     verbose_name=u'الاسم الأول')
+    ar_middle_name = models.CharField(max_length=30,
+                                      verbose_name=u'الاسم الأوسط')
+    ar_last_name = models.CharField(max_length=30,
+                                    verbose_name=u'الاسم الأخير')
+    mobile_number = models.CharField(max_length=20,
+                                     verbose_name=u'رقم الجوال')
+    picture = models.ImageField(upload_to=None)
+
+    def __unicode__(self):
+        return self.ar_first_name
+
+
+class Article(models.Model):
+    translator = models.ForeignKey(User, related_name='profile',blank=True)
+    en_name = models.CharField(max_length=100)
+    ar_name = models.CharField(max_length=100, blank=True)
+    addition_date = models.DateTimeField(auto_now_add=True)
+    en_link = models.URLField(max_length=200)
+    ar_link = models.URLField(max_length=200, blank=True)
+    when_accredit = models.DateTimeField(null=True, blank=True)
+    is_it_accredit = models.NullBooleanField()
+    is_it_translated = models.NullBooleanField()
+    when_translated = models.DateTimeField( null=True, blank=True)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+
+    def __unicode__(self):
+        return self.en_name
+
+class Activity(models.Model):
+    title=models.CharField(max_length=100)
+    date=models.DateField(auto_now_add=True)
+    description=models.TextField(max_length=200)
+    place=models.CharField(max_length=100)
+    start_time=models.DateTimeField(auto_now_add=True)
+    end_time=models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.title
