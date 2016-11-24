df11fb3c5060855442a1373e4f15c12cabdcda06
SadalKhalid <Sadal.14@hotmail.com>
2016-11-24 07:34:26 +0300

    various changes

--- Wiki/urls.py
+++ Wiki/urls.py
@@ -1,10 +1,12 @@
-from django.conf.urls import url, include
-from . import views
+from django.conf.urls import url
+from .views import HomeListView, ArticleDetailView, ArticleCreateView, \
+                   ArticleDeleteView, ArticleUpdateView
+
 
 urlpatterns = [
-
-
-
-
-
+    url(r'^$', HomeListView.as_view(), name="Wiki-home"),
+    url(r'^(?P<pk>\d+)/$', ArticleDetailView.as_view(), name="article-detail"),
+    url(r'^create/$', ArticleCreateView.as_view(), name="article-create"),
+    url(r'^delete/(?P<pk>\d+)/$', ArticleDeleteView.as_view(), name="article-delete"),
+    url(r'^update/(?P<pk>\d+)/$', ArticleUpdateView.as_view(), name="article-update"),
 ]
