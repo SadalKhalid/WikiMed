56ef3793b2e8582661f5b2b1a7c9bbe88c6a727a
SadalKhalid <Sadal.14@hotmail.com>
2016-11-24 07:32:40 +0300

    various changes

--- WikiMed/urls.py
+++ WikiMed/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from Wiki import views, models
+from Wiki import urls as wiki_urls
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^Wiki/', include(wiki_urls)),
 ]
