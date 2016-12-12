3b94d00021d5c0cff54b6f46ebda9f6464d901e3
SadalKhalid <Sadal.14@hotmail.com>
2016-12-12 20:30:05 +0300

    Various changes

--- Wiki/urls.py
+++ Wiki/urls.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import url
+from django.conf.urls import url, include
+from .forms import WikiSignupForm
+from userena import views as userena_views
+from userena import settings as userena_settings
+import django.contrib.auth.views
 from django.core.urlresolvers import reverse
 from .views import HomeListView, ArticleDetailView, ArticleCreateView, \
                    ArticleDeleteView, ArticleUpdateView, HomePageView,ProjectPageView ,\
-                   translating_page_view, Article_page
+                   translating_page_view, Article_page, Mark_Article_As_Finish
 
 
 urlpatterns = [
@@ -14,7 +18,13 @@ urlpatterns = [
     url(r'^Articles', HomeListView.as_view(), name="Wiki-home"),
     url(r'^(?P<pk>\d+)/$', ArticleDetailView.as_view(), name="article-detail"),
     url(r'^(?P<pk>\d+)/Article/$', Article_page.as_view(), name="article-page"),
+    url(r'^(?P<pk>\d+)/Article/FinishedArticles$', Mark_Article_As_Finish.as_view(),name="finished-article"),
     url(r'^create/$', ArticleCreateView.as_view(), name="article-create"),
     url(r'^delete/(?P<pk>\d+)/$', ArticleDeleteView.as_view(), name="article-delete"),
     url(r'^update/(?P<pk>\d+)/$', ArticleUpdateView.as_view(), name="article-update"),
+   # url(r'^accounts/signup/$', 'userena.views.signup', {'signup_form': WikiSignupForm, 'template_name': 'Wiki/signup_form.html'}),
+   # url(r'^accounts/', include('userena.urls')),
+  #  url(r'^accounts/resend/$', 'accounts.views.resend_confirmation_key', name='resend_confirmation_key'),
+   # url(r'^accounts/signin/$', 'userena.views.signin', {'auth_form': ModifiedAuthenticationForm}),
+  #  url(r'^accounts/edit/$', 'accounts.views.edit_common_profile', name='edit_common_profile'),
 ]
